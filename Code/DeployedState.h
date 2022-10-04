ad53ae7469010f7e5265813b4e8cbaec89e786c2
GT-GillesTeuwen <gillesteuwen@gmail.com>
2022-10-04 18:42:27 +0200

    Latest from dev branch

--- Code/DeployedState.h
+++ Code/DeployedState.h
@@ -9,7 +9,7 @@ class DeployedState : public UnitsState
 public:
 	virtual void handleDeployed() = 0;
 
-	virtual DeployedState * changeDeployedState() = 0;
+	virtual DeployedState *changeDeployedState() = 0;
 };
 
 #endif
