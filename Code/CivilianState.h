50532948348e6ed2841cc35ef2e0dcf2b1fe52f4
Inge505 <91600439+Inge505@users.noreply.github.com>
2022-10-06 09:18:03 +0200

    Updated CivilianState.h comments

--- Code/CivilianState.h
+++ Code/CivilianState.h
@@ -6,8 +6,25 @@ using namespace std;
 class CivilianState : public UnitsState
 {
     public: 
+        /**
+         * @brief Adjusts damage dealt based on UnitState
+         * 
+         * @param damage - the default damage dealt by the unit
+         * @return double - the damage dealt after adjustment
+         */
         virtual double attackStrength(double damage);
+
+        /**
+         * @brief Toggles to default next state (Deployed FIT)
+         * 
+         * @return UnitState * - a FitState() object
+         */
         virtual UnitsState * changeUnitState();
+
+        /**
+         * @brief Prints the current state of the unit
+         * 
+         */
         virtual void request();
 };
 
