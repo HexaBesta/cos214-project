f85a7416b55874e25bd81d5f234dd649a3626918
markbr02 <markbotros1@gmail.com>
2022-10-18 14:00:40 +0200

    Updated AreaVisitor - 2022/10/18

--- Code/Map/AreaVisitor.cpp
+++ Code/Map/AreaVisitor.cpp
@@ -1,9 +1,11 @@
 #include "AreaVisitor.h"
 
 void AreaVisitor::visit(Area* area){
-
+    cout<<"Area index: "<<area->getIndex()<<"\nArea Colour: "<<area->getColour()<<endl;
 };
 
 void AreaVisitor::visit(TransportRoute* transport){
+    Area* area1 = transport->getEndPoints();
 
+    cout<<"Transport route endpoints: "<<area1->getIndex()<<"\nTransport route colour: "<<area1->getColour()<endl;
 };
