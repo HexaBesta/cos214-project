1b4bb7f8e87fcdae3bc3a390b91d44433250442a
Inge505 <91600439+Inge505@users.noreply.github.com>
2022-10-21 22:46:33 +0200

    Added Transport Products

--- Code/TransportFactory/People.h
+++ Code/TransportFactory/People.h
@@ -8,6 +8,19 @@ using namespace std;
 
 class People : public Transport
 {
+    public:
+        /**
+         * @brief Construct a new People object
+         * 
+         * @param supply parameter for super constructor
+         */
+        People(int supply = 50);
+        /**
+         * @brief Returns an integer value of how much health will be improved
+         * 
+         * @return int - health improvement
+         */
+        int replenishHealth();
 
 };
 
