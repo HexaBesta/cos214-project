f08f73fd2f596416c5376c433d07b65b00016e24
Enchantress1416 <u21804312@tuks.co.za>
2022-10-27 12:45:00 +0200

    Print added to unit

--- Code/Unit/Unit.cpp
+++ Code/Unit/Unit.cpp
@@ -1,78 +1,96 @@
 #include "Unit.h"
 
-void Unit::requestState() {
-	if(this->unitState != NULL)
+void Unit::requestState()
+{
+	if (this->unitState != NULL)
 		this->unitState->request();
 }
 
-string Unit::getState(){
-	if(this->unitState != NULL){
+string Unit::getState()
+{
+	if (this->unitState != NULL)
+	{
 		return this->unitState->getState();
 	}
 	return "";
 }
 
-void Unit::setUnitState(UnitsState * unitState) {
-	if(unitState == nullptr){
+void Unit::setUnitState(UnitsState *unitState)
+{
+	if (unitState == nullptr)
+	{
 		this->unitState = this->unitState->changeUnitState();
-	}else{
+	}
+	else
+	{
 		delete this->unitState;
 		this->unitState = unitState;
 	}
-	
 }
 
-int Unit::getDamage(){
+int Unit::getDamage()
+{
 	return this->damage;
 }
 
-int Unit::getHealth(){
+int Unit::getHealth()
+{
 	return this->health;
 }
 
-bool Unit::takeDamage(int damage) {
+bool Unit::takeDamage(int damage)
+{
 	this->health = this->health - damage;
-	if(this->health <= 0 ){
+	if (this->health <= 0)
+	{
 		this->setUnitState(new DeadState());
 		return true;
 	}
 	return false;
 }
 
- Unit*  Unit::split(){return NULL;}
+Unit *Unit::split() { return NULL; }
 
+void Unit::join(Unit *others) {}
 
-void  Unit::join(Unit* others){}
-
-string Unit::getBranch(){
+string Unit::getBranch()
+{
 	return "NotAClue";
 }
 
-string Unit::getAlliance(){
+string Unit::getAlliance()
+{
 	return country->getAlliances()->getName();
 }
 
-void Unit::setCountry(Country* country){
+void Unit::setCountry(Country *country)
+{
 	this->country = country;
 }
 
-Country* Unit::getCountry(){
-    return this->country;
+Country *Unit::getCountry()
+{
+	return this->country;
 }
 
-void Unit::print(){
-	
-	cout<<"Add unit print statement"<<endl;
+void Unit::print()
+{
+
+	cout << "Current State of " << getCountry() << ": "<< getState() << endl
+		 << "Current Health: " << getHealth() << endl
+		 << "Damage sustained: " << getDamage() << endl;
+
+	// cout<<"Add unit print statement"<<endl;
 }
 
-Unit* Unit::takeRandom(){
+Unit *Unit::takeRandom()
+{
 	return this;
 }
 
-Unit::~Unit(){
-	
+Unit::~Unit()
+{
+
 	delete this->unitState;
-    //delete this->country;
+	// delete this->country;
 }
-
-
