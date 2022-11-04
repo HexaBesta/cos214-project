bc126879c533a1c5ff1856bd65d3848e37967e82
Inge505 <91600439+Inge505@users.noreply.github.com>
2022-11-04 15:44:47 +0200

    Fixed it was 0 again... - 2022/11/04

--- Code/CPU.cpp
+++ Code/CPU.cpp
@@ -343,7 +343,7 @@ Player *CPU::togglePlayer()
 
 void CPU::createCountries(Map *map)
 {
-    srand(0);
+    srand(5);
     int ranNum = rand() % ((8)) + 6;
     cout << "--------------------------------------------" << endl;
     cout << "Creating " << ranNum << " countries" << endl;
